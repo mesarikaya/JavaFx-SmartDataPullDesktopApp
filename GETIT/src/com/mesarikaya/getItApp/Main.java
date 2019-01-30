8705fa27953e50cc5e0ab2319112b1398a42a727
Ergin Sarikaya <mesarikaya@users.noreply.github.com>
2019-01-30 10:44:35 +0100

    Update Main.java

--- GETIT/src/com/mesarikaya/getItApp/Main.java
+++ GETIT/src/com/mesarikaya/getItApp/Main.java
@@ -2,12 +2,11 @@ package com.mesarikaya.getItApp;
 
 import com.mesarikaya.getItApp.Controllers.LoginController;
 import com.mesarikaya.getItApp.Controllers.UserPageController;
+import com.mesarikaya.getItApp.Services.ScreenService;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -16,15 +15,16 @@ public class Main extends Application {
 
     private static LoginController loginController;
     private static UserPageController userPageController;
-    private double xOffset = 0;
-    private double yOffset = 0;
+    private static double xOffset = 0;
+    private static double yOffset = 0;
+    private ScreenService screenService;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
         // Set the login page scene
         FXMLLoader firstPaneLoader = new FXMLLoader(getClass().getResource("login.fxml"));
-        Parent firstPane = firstPaneLoader.load();
+        Pane firstPane = firstPaneLoader.load();
         Scene firstScene = new Scene(firstPane);
 
         // Set the user page scene
@@ -33,37 +33,22 @@ public class Main extends Application {
         // injecting second scene into the controller of the first scene
         LoginController firstPaneController = (LoginController) firstPaneLoader.getController();
         firstPaneController.setSecondPageLoader(secondPageLoader);
+        firstPaneController.setPrimaryStage(primaryStage);
+        firstPaneController.setScreenDraggable(true); // Enable screen dragging
 
-        firstPane.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                xOffset = event.getSceneX();
-                yOffset = event.getSceneY();
-            }
-        });
-
-        firstPane.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                primaryStage.setX(event.getScreenX() - xOffset);
-                primaryStage.setY(event.getScreenY() - yOffset);
-            }
-        });
-
+        primaryStage.setResizable(true);
         primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setTitle("GeTIt");
-
-        primaryStage.setResizable(false);
         primaryStage.setScene(firstScene);
 
         firstScene.setFill(Color.valueOf("#2F3739"));
+        primaryStage.setMaxHeight(400);
+        primaryStage.setMaxWidth(400);
+
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
     }
-
-
 }
